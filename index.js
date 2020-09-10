2294e279306628c97992ffd7997287a0f8b1247c
Morteza Aliyari <aliyari@outlook.com>
2020-09-10 21:57:46 +0430

    init server in index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const { PORT, MONGO_URI, MONGO_URI_DEV } = require("./config");
+
+const typeDefs = require("./src/graphql/typeDefs");
+const resolvers = require("./src/graphql/resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req })
+});
+
+try {
+  server.listen({ port: PORT }).then(({ url }) => {
+    console.log(`Server is running on ${url}`);
+    return mongoose
+      .connect(MONGO_URI_DEV, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      })
+      .then(() => {
+        console.log(`Database connected successfuly.`);
+      });
+  });
+} catch (error) {
+  throw new Error(error);
+}
